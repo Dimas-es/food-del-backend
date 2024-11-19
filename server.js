f34dfba118b6371efe0808938d3ae4865e7fcefc
Bindery <evosdimm@gmail.com>
2024-11-19 08:36:58 +0700

    deploy 2

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ import orderRouter from "./routes/orderRoute.js"
 import helmet from 'helmet'
 import mongoSanitize from 'express-mongo-sanitize'
 
-
-
-
 // app config
 const app = express()
 const port = process.env.PORT || 4000
@@ -20,23 +17,34 @@ const port = process.env.PORT || 4000
 app.use(helmet())
 app.use(mongoSanitize())
 app.use(express.json())
-app.use(cors());
+
+// Configure CORS for only your frontend
+app.use(cors({
+  origin: 'http://localhost:5173', // Ganti dengan URL frontend Anda
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Add CORP header to all responses
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');  // Atau 'same-origin' jika diperlukan
+  next();
+});
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))  // static folder for images
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
+app.get("/", (req, res) => {
+  res.send("API Working")
 })
 
-app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`)
 })
-
